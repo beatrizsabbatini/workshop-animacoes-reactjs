a277d531084bc9f567c96855c4d89d096e21779c
Beatriz Schwartz Sabbatini <43048941+beatrizsabbatini@users.noreply.github.com>
2021-03-12 01:05:16 -0300

    feat: added help comment

--- src/components/ParallaxWord/index.tsx
+++ src/components/ParallaxWord/index.tsx
@@ -15,6 +15,7 @@ interface Item {
 const ParallaxWord: React.FC<ParallaxWordProps> = ({ item, children }) => {
   const { scrollY } = useViewportScroll();
   const yPosition = useTransform(scrollY, item.yValues, [0, -600]); // (posição atual no scroll, inputRange, outputRange)
+  // o valor negativo no -600, faz os itens subirem, se foose positivo (600), o efeito parallax seria pra baixo
 
   return (
     <motion.h1 style={{ y: yPosition }} animate={{ scale: item.scale }}>
