facaf0959553035a59981bb0e19e9259d37a83d4
Beatriz Schwartz Sabbatini <43048941+beatrizsabbatini@users.noreply.github.com>
2021-03-11 22:32:41 -0300

    fix: fixed techs screen height

--- src/pages/Techs/styles.ts
+++ src/pages/Techs/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ParallaxContainer = styled.div`
   width: 100%;
-  height: 100%;
+  height: 100vh;
   position: relative;
   overflow: hidden;
 
